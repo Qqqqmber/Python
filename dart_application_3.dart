bc5168e4a9b24a1e9ef0d9376eaf0e67cdc28776
Сергей Алексеевич Т. <a89774575049@gmail.com>
2026-04-02 10:41:19 +0300

    Add files via upload

--- dart_application_3.dart
+++ dart_application_3.dart
@@ -3,10 +3,10 @@ import 'dart:io';
 import 'package:characters/characters.dart';
 
 enum Mood {
-  happy('Счастье', '\u{1F600}', 10),
-  sad('Грусть', '\u{1F622}', 3),
+  happy('Счастье', '\u{1F601}', 10),
+  sad('Грусть', '\u{1F623}', 3),
   boredom('Скука', '\u{1F611}', 2),
-  disease('Боль', '\u{1F912}', 7),
+  disease('Боль', '\u{1F916}', 7),
   anxiety('Тревога', '\u{1F628}', 4);
 
   final String description;
@@ -14,47 +14,39 @@ enum Mood {
   final int energy;
 
   const Mood(this.description, this.emoji, this.energy);
+
   static Mood getRandom() => values[Random().nextInt(values.length)];
 }
 
 void main() {
   stdout.write('Введите ваше имя: ');
   String name = stdin.readLineSync() ?? 'Гость';
-  print('\nГенерируем случайное настроение...');
-  
+  print('\nГенерируем случайное настроени');
   Mood mood = Mood.getRandom();
-
   print('Привет, $name! Твое настроение: ${mood.emoji} ${mood.description} (энергия: ${mood.energy}/10)');
 
   int codePoint = mood.emoji.runes.first;
   print('\nЮникод вашего эмодзи: U+${codePoint.toRadixString(16).toUpperCase()}');
 
   stdout.write('\nХотите просмотреть сложные эмодзи? (yes/no): ');
-  String answer = stdin.readLineSync()?.toLowerCase() ?? 'нет';
-
-  if (answer == 'yes' || answer == 'y') {
+  String choice = stdin.readLineSync()?.toLowerCase() ?? '';
+  if (choice == "yes") {
     stdout.write('Введите комбинацию эмодзи: ');
-    String input = stdin.readLineSync() ?? '';
+    String emodzi_combination = stdin.readLineSync() ?? '';
 
-    int utf16Units = input.length;
-    int codePoints = input.runes.length;
-    int realChars = input.characters.length;
+    print("\n16 битных единиц: ${emodzi_combination.length}");
+    print("Кодовых точек: ${emodzi_combination.runes.length}");
+    print("Реальных символов: ${emodzi_combination.characters.length}");
 
-    print('\nАнализ строки "$input":');
-    print('- 16-битных единиц: $utf16Units');
-    print('- Кодовых точек: $codePoints');
-    print('- Реальных символов: $realChars');
-    print('\nПодробный вывод юникода:');
-    int index = 1;
-    for (var char in input.characters) {
-      List<String> codePointsList = [];
-      for (var rune in char.runes) {
-        codePointsList.add('U+${rune.toRadixString(16).toUpperCase()}');
-      }
-      print('Символ $index: $char → ${codePointsList.join(' ')}');
-      index++;
+    print("\nПодробный вывод юникода:");
+    int count = 0;
+    for (var rune in emodzi_combination.runes) {
+      count++;
+      print("Символ $count: ${String.fromCharCode(rune)}  ->  U+${rune.toRadixString(16).toUpperCase()}");
     }
+    print("Хорошего дня");
   }
-
-  print('\nСпасибо, приходите снова!');
+  else {
+    print('Пока!');
+  }
 }
